fda5a7987c97d38b4e3ff92c0d7ccec9da9284a2
LudmillaPotel <ludmillapotel@hotmail.com>
2023-08-25 14:29:11 -0300

    ProductDetails route

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -3,20 +3,29 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "components/Navbar";
 import Catalog from "pages/Catalog";
 import Admin from "pages/Admin";
+import ProductDetails from "pages/ProductDetails";
 
 const Routes = () => (
     <BrowserRouter>
         <Navbar />
         <Switch>
+
             <Route path="/" exact>
                 <Home />
             </Route>
-            <Route path="/products">
+
+            <Route path="/products" exact>
                 <Catalog />
             </Route>
+
+            <Route path="/products/:productId">
+                <ProductDetails />
+            </Route>
+
             <Route path="/admin">
                 <Admin />
             </Route>
+
         </Switch>
     </BrowserRouter>
 );
